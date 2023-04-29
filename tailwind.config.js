63cf48a0043e55c5eb50111e0b41f1acf95fe879
phaetton <31552767+phaetton@users.noreply.github.com>
2023-04-29 05:19:09 -0600

    creando vista previa primer parte

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+      'minism': '0.4rem',
+      'minibase': '0.6rem',
+      'minilg': '0.8rem',
+      'minixl': '0.9rem',
+    }
   },
   plugins: [],
 }
