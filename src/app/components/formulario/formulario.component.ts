d4624f08a13491eab9f7e10d5eff99cee660bea2
phaetton <31552767+phaetton@users.noreply.github.com>
2023-05-06 12:33:41 -0600

    Actualizando trabajo

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -37,12 +37,12 @@ export class FormularioComponent implements OnInit {
       hobbies: this.fb.array([]),
       habilidades: this.fb.array([]),
       idiomas: this.fb.array([]),
-      nombre: new FormControl("Rafael Francisco"),
-      apellidos: new FormControl("Serrano Urtecho"),
-      posicionlaboral: new FormControl("Programador Web"),
-      telefono: new FormControl("612471014"),
-      email: new FormControl("rafa.tesla@gmail.com"),
-      perfilprofesional: new FormControl("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis posuere massa arcu, eu porttitor est tincidunt sed. Integer sit amet volutpat metus, ac varius velit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel quam ultricies, venenatis risus sit amet, mollis justo. Aenean ut convallis velit. Vivamus interdum rhoncus mauris non convallis. Duis in nibh porta nunc dictum tristique. Fusce tristique maximus risus, eu sollicitudin risus interdum ut. Vestibulum malesuada vitae arcu sed congue.  Etiam molestie dignissim odio sollicitudin fermentum. Sed risus ex, cursus ac purus non, placerat bibendum neque. Phasellus pharetra nec augue sit amet finibus. Ut condimentum sem in dignissim tristique. In sit amet sagittis magna. Nam id tincidunt ante. Donec porttitor ante arcu, nec porta diam faucibus quis. Cras fringilla libero nec cursus volutpat. Nullam feugiat nulla lorem, vitae vestibulum augue bibendum sit amet. Nullam odio neque, viverra et nisi non, elementum tempus turpis. Mauris et bibendum eros. Maecenas porttitor congue eros sed gravida. Cras condimentum, turpis vel varius tincidunt, leo massa vehicula dui, eu ultrices quam libero vitae lacus.  Cras magna metus, consequat eu diam ac, tincidunt faucibus risus. Curabitur id eros nec purus bibendum cursus quis quis purus. Nulla neque tellus, congue ut sollicitudin ut, mollis id sem. Praesent ex quam, vulputate at enim id, sodales egestas augue. Mauris interdum justo venenatis, molestie mi vel, euismod mi. Vivamus luctus, libero eu luctus mollis, ligula elit malesuada mauris, sed dictum augue ante et erat. Vivamus sed urna eu arcu placerat pretium eu nec diam. Nulla suscipit lectus ut mauris porta, ut congue felis aliquam. Nullam consectetur dui sit amet maximus tempor. Nam pellentesque rhoncus sapien. Etiam vestibulum scelerisque tempor. Maecenas auctor, magna a laoreet suscipit, purus velit sollicitudin ipsum, eget venenatis odio ipsum ac augue."),
+      nombre: new FormControl(""),
+      apellidos: new FormControl(""),
+      posicionlaboral: new FormControl(""),
+      telefono: new FormControl(""),
+      email: new FormControl(""),
+      perfilprofesional: new FormControl(""),
 
     });
   }
@@ -143,7 +143,7 @@ export class FormularioComponent implements OnInit {
       instituto: new FormControl(''),
       curso: new FormControl(''),
       fechainicio: new FormControl(''),
-      fechafin: new FormControl(''),  
+      fechafin: new FormControl(''),
     })
     this.cursosrealizados.push(cursosrealizadosFg);
   }
